af09bc4bfb753e753b3d515fbe3917b7b1cf998b
Aaron J Arthurs <aajarthurs@gmail.com>
2019-08-19 14:55:56 -0500

    Specify display for Xterm

--- docker_entrypoint.bash
+++ docker_entrypoint.bash
@@ -2,4 +2,4 @@
 #vncserver -xstartup /usr/bin/xterm -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE -geometry 1024x720 -depth 32 :0
 vncserver -xstartup /usr/bin/startlxde -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE -geometry 1024x720 -depth 32 :0
 
-xterm
+DISPLAY=:0 xterm
