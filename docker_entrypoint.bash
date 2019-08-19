5a506c5ab037b3d1772e983dcb1d25491a121569
Aaron J Arthurs <aajarthurs@gmail.com>
2019-08-19 14:42:21 -0500

    Setup desktop environment to start Xterm in LXDE

--- docker_entrypoint.bash
+++ docker_entrypoint.bash
@@ -1,4 +1,4 @@
 #!/bin/sh
-vncserver -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE -geometry 1024x720 -depth 32 :0
+vncserver -xstartup /usr/bin/startlxde -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE -geometry 1024x720 -depth 32 :0
 
-/bin/bash
+xterm&
