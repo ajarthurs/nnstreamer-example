7916cdfa8b1029cd8abeec955a0d2e13d49d9c7a
Aaron J Arthurs <aajarthurs@gmail.com>
2019-08-19 14:50:02 -0500

    Do not background Xterm

--- docker_entrypoint.bash
+++ docker_entrypoint.bash
@@ -1,4 +1,5 @@
 #!/bin/sh
+#vncserver -xstartup /usr/bin/xterm -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE -geometry 1024x720 -depth 32 :0
 vncserver -xstartup /usr/bin/startlxde -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE -geometry 1024x720 -depth 32 :0
 
-xterm&
+xterm
