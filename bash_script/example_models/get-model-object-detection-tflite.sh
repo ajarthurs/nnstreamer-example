96f9710a01471a9a160614a9fd4c5306d821b2cc
Aaron J Arthurs <aajarthurs@gmail.com>
2019-08-21 15:45:53 -0500

    Switch to Dropbox to fetch sample video

--- bash_script/example_models/get-model-object-detection-tflite.sh
+++ bash_script/example_models/get-model-object-detection-tflite.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Sample video from Coverr@pexels.com showing Broadway Street (16 sec).
-wget 'https://gcs-vimeo.akamaized.net/exp=1566417848~acl=%2A%2F792677904.mp4%2A~hmac=b5e784c9702a2e2c98fc0fcddce6495e3549e77fcdada13b74a3ce379f698719/vimeo-prod-skyfire-std-us/01/159/9/225795843/792677904.mp4?download=1&filename=Pexels+Videos+4585.mp4' -O sample_1080p.mp4
+wget 'https://www.dropbox.com/s/v2knofo4z19ggue/sample_1080p.mp4?dl=0' -O sample_1080p.mp4
 
 # Model
 mkdir -p tflite_model
