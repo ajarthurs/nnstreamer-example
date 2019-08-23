eaf361ab15fee4623a4124d5276f236a92645d38
Aaron J Arthurs <aajarthurs@gmail.com>
2019-08-23 10:45:59 -0500

    Disable screen-lock during VNC session

--- docker_entrypoint.bash
+++ docker_entrypoint.bash
@@ -1,5 +1,6 @@
 #!/bin/sh
 #vncserver -xstartup /usr/bin/xterm -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE -geometry 1024x720 -depth 32 :0
 vncserver -xstartup /usr/bin/startlxde -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE -geometry 1024x720 -depth 32 :0
+pkill light-locker
 
 DISPLAY=:0 xterm
